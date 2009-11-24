64f7924800b491c598d436fc873f52b41bb8370d
Yuya Kato <yuyakato@gmail.com>
2009-11-24 18:19:37 +0900

    URL正規化処理を追加

--- python/lib/bookmark_utility/__init__.py
+++ python/lib/bookmark_utility/__init__.py
@@ -5,5 +5,13 @@ import re
 class BookmarkUtility:
   @classmethod
   def get_canonical_url(cls, url):
-    p = re.compile(r"\A(http://journal\.mycom\.co\.jp/.+)\?rt=na\Z")
-    return re.sub(p, r"\1index.html", url)
+    url = re.sub(r"\A(http:\/\/[a-z]+\.yomiuri\.co\.jp\/.+)\?from=[a-z0-9]+\Z", r"\1", url)
+    url = re.sub(r"\A(http:\/\/journal\.mycom\.co\.jp\/.+)\?rt=na\Z", r"\1index.html", url)
+    url = re.sub(r"\A(http:\/\/mainichi\.jp\/.+)\?link_id=[A-Z0-9]+\Z", r"\1", url)
+    url = re.sub(r"\A(http:\/\/news\.searchina\.ne\.jp\/.+)&pt=large\Z", r"\1", url)
+    url = re.sub(r"\A(http:\/\/www\.asahi\.com\/.+)\?ref=rss\Z", r"\1", url)
+    url = re.sub(r"\A(http:\/\/www\.chunichi\.co\.jp\/.+)\?ref=rank\Z", r"\1", url)
+    url = re.sub(r"\A(http:\/\/www\.jiji\.com\/jc\/c\?g=.+?)&rel=j7(&k=\d+)\Z", r"\1\2", url)
+    url = re.sub(r"\A(http:\/\/www\.tetsudo\.com\/.+)\?tag=as\.rss\Z", r"\1", url)
+
+    return url
