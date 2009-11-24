c2c2f609c6671c972fdef040bc1137a126a01144
Yuya Kato <yuyakato@gmail.com>
2009-11-24 17:04:47 +0900

    URL正規化のパターンをファイルに切り出し

---
+++ ruby/lib/bookmark_utility/canonical.rb
@@ -0,0 +1,13 @@
+
+module BookmarkUtility
+  CANONICAL_TABLE = [
+    [/\A(http:\/\/[a-z]+\.yomiuri\.co\.jp\/.+)\?from=[a-z0-9]+\z/, '\1'],
+    [/\A(http:\/\/journal\.mycom\.co\.jp\/.+)\?rt=na\z/, '\1index.html'],
+    [/\A(http:\/\/mainichi\.jp\/.+)\?link_id=[A-Z0-9]+\z/, '\1'],
+    [/\A(http:\/\/news\.searchina\.ne\.jp\/.+)&pt=large\z/, '\1'],
+    [/\A(http:\/\/www\.asahi\.com\/.+)\?ref=rss\z/, '\1'],
+    [/\A(http:\/\/www\.chunichi\.co\.jp\/.+)\?ref=rank\z/, '\1'],
+    [/\A(http:\/\/www\.jiji\.com\/jc\/c\?g=.+?)&rel=j7(&k=\d+)\z/, '\1\2'],
+    [/\A(http:\/\/www\.tetsudo\.com\/.+)\?tag=as\.rss/, '\1'],
+  ].freeze.each { |a| a.freeze }
+end
