ea70b382f6c6da04ea0fa3950b1d9047b8ef9968
Yuya Kato <yuyakato@gmail.com>
2009-11-24 17:20:34 +0900

    BookmarkUtilityクラスを追加

---
+++ python/test/canonical_test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import sys
+sys.path.append("python/lib") # FIXME:
+
+import unittest
+from bookmark_utility import BookmarkUtility
+
+class TestCanonical(unittest.TestCase):
+  def setUp(self):
+    pass
+
+if __name__ == "__main__":
+  unittest.main()
