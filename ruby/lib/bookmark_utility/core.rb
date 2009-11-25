41e600120104a7f201c5263ff9cf3c772e9fb38a
Yuya Kato <yuyakato@gmail.com>
2009-11-25 19:15:55 +0900

    リファクタリング

--- ruby/lib/bookmark_utility/core.rb
+++ ruby/lib/bookmark_utility/core.rb
@@ -1,5 +1,6 @@
 
 require File.join(File.dirname(__FILE__), "canonical_table")
+require File.join(File.dirname(__FILE__), "filter_table")
 
 module BookmarkUtility
   def self.get_canonical_url(url)
@@ -13,14 +14,9 @@ module BookmarkUtility
   end
 
   def self.reject?(url)
-    return true if %r'\Ahttp://car\.nikkei\.co\.jp/release/' =~ url
-    return true if %r'\Ahttp://mainichi\.jp/life/money/kabu/nsj/news/' =~ url
-    return true if %r'\Ahttp://markets\.nikkei\.co\.jp/' =~ url
-    return true if %r'\Ahttp://www\.nikkei\.co\.jp/kyushu/news/' =~ url
-    return true if %r'\Ahttp://www\.nikkei\.co\.jp/news/' =~ url
-    return true if %r'\Ahttp://www\.pjnews\.net/' =~ url
-    return true if %r'\Ahttp://www\.travelvision\.jp/' =~ url
-    return true if %r'\Ahttp://www\.wbs\.co\.jp/' =~ url
+    FilterTable.each { |pattern|
+      return true if pattern =~ url
+    }
     return false
   end
 end
---
+++ ruby/lib/bookmark_utility/filter_table.rb
@@ -0,0 +1,13 @@
+
+module BookmarkUtility
+  FilterTable = [
+    %r'\Ahttp://car\.nikkei\.co\.jp/release/',
+    %r'\Ahttp://mainichi\.jp/life/money/kabu/nsj/news/',
+    %r'\Ahttp://markets\.nikkei\.co\.jp/',
+    %r'\Ahttp://www\.nikkei\.co\.jp/kyushu/news/',
+    %r'\Ahttp://www\.nikkei\.co\.jp/news/',
+    %r'\Ahttp://www\.pjnews\.net/',
+    %r'\Ahttp://www\.travelvision\.jp/',
+    %r'\Ahttp://www\.wbs\.co\.jp/',
+  ].freeze
+end
