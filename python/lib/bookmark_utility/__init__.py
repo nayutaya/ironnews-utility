2dff258c3a3b02940e89c20909295d130d175ba2
Yuya Kato <yuyakato@gmail.com>
2009-11-25 11:29:44 +0900

    URL正規化のためのパターンをファイルに切り出し

--- python/lib/bookmark_utility/__init__.py
+++ python/lib/bookmark_utility/__init__.py
@@ -2,22 +2,13 @@
 
 import re
 
-class BookmarkUtility:
-  CANONICAL_TABLE = [
-    (re.compile(r"\A(http://[a-z]+\.yomiuri\.co\.jp/.+)\?from=[a-z0-9]+\Z"), r"\1"),
-    (re.compile(r"\A(http://journal\.mycom\.co\.jp/.+)\?rt=na\Z"), r"\1index.html"),
-    (re.compile(r"\A(http://mainichi\.jp/.+)\?link_id=[A-Z0-9]+\Z"), r"\1"),
-    (re.compile(r"\A(http://news\.searchina\.ne\.jp/.+)&pt=large\Z"), r"\1"),
-    (re.compile(r"\A(http://www\.asahi\.com/.+)\?ref=rss\Z"), r"\1"),
-    (re.compile(r"\A(http://www\.chunichi\.co\.jp/.+)\?ref=rank\Z"), r"\1"),
-    (re.compile(r"\A(http://www\.jiji\.com/jc/c\?g=.+?)&rel=j7(&k=\d+)\Z"), r"\1\2"),
-    (re.compile(r"\A(http://www\.tetsudo\.com/.+)\?tag=as\.rss\Z"), r"\1"),
-  ]
+from canonical_table import CanonicalTable
 
+class BookmarkUtility:
   @classmethod
   def get_canonical_url(cls, url):
     url1 = url2 = url
-    for (pattern, replace) in cls.CANONICAL_TABLE:
+    for (pattern, replace) in CanonicalTable:
       url2 = re.sub(pattern, replace, url1)
       if url2 != url1: break
       url1 = url2
--- python/test/canonical_test.py
+++ python/test/canonical_test.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-import os
-import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), "..", "lib"))
-
 import unittest
 import re
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "lib"))
 from bookmark_utility import BookmarkUtility
 
 class TestCanonical(unittest.TestCase):
