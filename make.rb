b1a8f770223754f6760a276241406c32fed3f283
Yuya Kato <yuyakato@gmail.com>
2009-11-25 11:35:44 +0900

    Python用のURL正規化パターンを生成する処理を追加

--- make.rb
+++ make.rb
@@ -18,3 +18,17 @@ File.open("ruby/lib/bookmark_utility/canonical.rb", "wb") { |file|
   file.puts("  ].freeze.each { |a| a.freeze }")
   file.puts("end")
 }
+
+File.open("python/lib/bookmark_utility/canonical_table.py", "wb") { |file|
+  file.puts("# -*- coding: utf-8 -*-")
+  file.puts("")
+  file.puts("import re")
+  file.puts("")
+  file.puts("CanonicalTable = [")
+
+  patterns.each { |pattern, replace|
+    file.printf(%|  (re.compile(r"%s"), r"%s"),\n|, pattern, replace)
+  }
+
+  file.puts("]")
+}
