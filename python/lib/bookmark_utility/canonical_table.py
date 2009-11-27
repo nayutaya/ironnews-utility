8179f2c684dd7596160530d4a1c43536b299214e
Yuya Kato <yuyakato@gmail.com>
2009-11-27 11:55:20 +0900

    URL正規化パターンを修正

--- python/lib/bookmark_utility/canonical_table.py
+++ python/lib/bookmark_utility/canonical_table.py
@@ -8,9 +8,9 @@ CanonicalTable = (
   (re.compile(r'\A(http://journal\.mycom\.co\.jp/.+)\?rt=na\Z'), r'\1index.html'),
   (re.compile(r'\A(http://mainichi\.jp/.+)\?link_id=[A-Z0-9]+\Z'), r'\1'),
   (re.compile(r'\A(http://news\.searchina\.ne\.jp/.+)&pt=large\Z'), r'\1'),
-  (re.compile(r'\A(http://slashdot.jp/.+)\.shtml\Z'), r'\1'),
   (re.compile(r'\A(http://www\.asahi\.com/.+)\?ref=rss\Z'), r'\1'),
   (re.compile(r'\A(http://www\.chunichi\.co\.jp/.+)\?ref=rank\Z'), r'\1'),
   (re.compile(r'\A(http://www\.jiji\.com/jc/c\?g=.+?)&rel=j7(&k=\d+)\Z'), r'\1\2'),
   (re.compile(r'\A(http://www\.tetsudo\.com/.+)\?tag=as\.rss\Z'), r'\1'),
+  (re.compile(r'\Ahttp://slashdot\.jp/articles/(\d+/\d+/\d+/\d+)\.shtml'), r'http://slashdot.jp/article.pl?sid=\1'),
 )
