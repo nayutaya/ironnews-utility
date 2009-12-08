3ed8a77e1b994ce530c17257c647cfa09bfb2ed1
Yuya Kato <yuyakato@gmail.com>
2009-12-08 14:16:34 +0900

    URL正規化パターンを更新

--- ruby/lib/bookmark_utility/canonical_table.rb
+++ ruby/lib/bookmark_utility/canonical_table.rb
@@ -11,6 +11,7 @@ module BookmarkUtility
     [%r'\A(http://www\.asahi\.com/.+)\?ref=rss\Z', '\1'].freeze,
     [%r'\A(http://www\.chunichi\.co\.jp/.+)\?ref=rank\Z', '\1'].freeze,
     [%r'\A(http://www\.jiji\.com/.+)&j1\Z', '\1'].freeze,
+    [%r'\A(http://www\.jiji\.com/.+)&n=1\Z', '\1'].freeze,
     [%r'\A(http://www\.jiji\.com/jc/c\?g=.+?)&rel=j7(&k=\d+)\Z', '\1\2'].freeze,
     [%r'\A(http://www\.tetsudo\.com/.+)\?tag=as\.rss\Z', '\1'].freeze,
     [%r'\Ahttp://slashdot\.jp/articles/(\d+/\d+/\d+/\d+)\.shtml', 'http://slashdot.jp/article.pl?sid=\1'].freeze,
