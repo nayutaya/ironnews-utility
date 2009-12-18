d62c4113ffb3ba81c6b597acf28c2620c352d5f4
Yuya Kato <yuyakato@gmail.com>
2009-12-18 13:52:59 +0900

    フィルタパターンを更新

--- python/lib/bookmark_utility/filter_table.py
+++ python/lib/bookmark_utility/filter_table.py
@@ -5,6 +5,7 @@ import re
 FilterTable = (
   re.compile(r'\Ahttp://car\.nikkei\.co\.jp/news/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://car\.nikkei\.co\.jp/release/'), # 理由: title要素に記事名が含まれていないため
+  re.compile(r'\Ahttp://cargo-news\.co\.jp/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://contents\.innolife\.net/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://japan\.donga\.com/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://japanese\.donga\.com/'), # 理由: title要素に記事名が含まれていないため
@@ -19,6 +20,7 @@ FilterTable = (
   re.compile(r'\Ahttp://www\.chugoku-np\.co\.jp/News/Tn'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://www\.hab\.co\.jp/headline/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://www\.iwate-np\.co\.jp/'), # 理由: title要素に記事名が含まれていないため
+  re.compile(r'\Ahttp://www\.japanjournals\.com/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://www\.keidanren\.or\.jp/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://www\.kentsu\.co\.jp/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://www\.mxtv\.co\.jp/mxnews/news/'), # 理由: title要素に記事名が含まれていないため
@@ -26,8 +28,12 @@ FilterTable = (
   re.compile(r'\Ahttp://www\.nikkei\.co\.jp/kyushu/news/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://www\.nikkei\.co\.jp/news/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://www\.pjnews\.net/'), # 理由: title要素に記事名が含まれていないため
+  re.compile(r'\Ahttp://www\.portfolio\.nl/'), # 理由: title要素に記事名が含まれていないため
+  re.compile(r'\Ahttp://www\.shonai-nippo\.co\.jp/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://www\.tohkaishimpo\.com/'), # 理由: title要素に記事名が含まれていないため
+  re.compile(r'\Ahttp://www\.toyama\.hokkoku\.co\.jp/subpage/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://www\.travelvision\.jp/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://www\.usfl\.com/Daily/News/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://www\.wbs\.co\.jp/'), # 理由: title要素に記事名が含まれていないため
+  re.compile(r'\Ahttp://www\.xinhua\.jp/'), # 理由: 記事の閲覧が有料であるため
 )
--- ruby/lib/bookmark_utility/filter_table.rb
+++ ruby/lib/bookmark_utility/filter_table.rb
@@ -3,6 +3,7 @@ module BookmarkUtility
   FilterTable = [
     %r'\Ahttp://car\.nikkei\.co\.jp/news/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://car\.nikkei\.co\.jp/release/', # 理由: title要素に記事名が含まれていないため
+    %r'\Ahttp://cargo-news\.co\.jp/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://contents\.innolife\.net/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://japan\.donga\.com/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://japanese\.donga\.com/', # 理由: title要素に記事名が含まれていないため
@@ -17,6 +18,7 @@ module BookmarkUtility
     %r'\Ahttp://www\.chugoku-np\.co\.jp/News/Tn', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://www\.hab\.co\.jp/headline/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://www\.iwate-np\.co\.jp/', # 理由: title要素に記事名が含まれていないため
+    %r'\Ahttp://www\.japanjournals\.com/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://www\.keidanren\.or\.jp/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://www\.kentsu\.co\.jp/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://www\.mxtv\.co\.jp/mxnews/news/', # 理由: title要素に記事名が含まれていないため
@@ -24,9 +26,13 @@ module BookmarkUtility
     %r'\Ahttp://www\.nikkei\.co\.jp/kyushu/news/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://www\.nikkei\.co\.jp/news/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://www\.pjnews\.net/', # 理由: title要素に記事名が含まれていないため
+    %r'\Ahttp://www\.portfolio\.nl/', # 理由: title要素に記事名が含まれていないため
+    %r'\Ahttp://www\.shonai-nippo\.co\.jp/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://www\.tohkaishimpo\.com/', # 理由: title要素に記事名が含まれていないため
+    %r'\Ahttp://www\.toyama\.hokkoku\.co\.jp/subpage/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://www\.travelvision\.jp/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://www\.usfl\.com/Daily/News/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://www\.wbs\.co\.jp/', # 理由: title要素に記事名が含まれていないため
+    %r'\Ahttp://www\.xinhua\.jp/', # 理由: 記事の閲覧が有料であるため
   ].freeze
 end
