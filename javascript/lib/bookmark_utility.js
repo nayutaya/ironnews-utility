2eaa53b2b401a9a9d825e35b996517bcf58aa433
Yuya Kato <yuyakato@gmail.com>
2010-01-07 18:10:36 +0900

    リファクタリング

--- javascript/lib/bookmark_utility.js
+++ javascript/lib/bookmark_utility.js
@@ -1,13 +1,13 @@
 
 var BookmarkUtility = {};
 
+BookmarkUtility.HostPattern = /^http:\/\/(.+?)[\/:]/;
+
 BookmarkUtility.cleanse_title = function(url, title) {
 console.debug(url);
-  var match = url.match(/^http:\/\/(.+?)[\/:]/);
-console.debug(match);
-  if ( match != null )
+  var host = (url.match(BookmarkUtility.HostPattern) || [])[1];
+  if ( host != null )
   {
-    var host = match[1];
     if ( host == "mainichi.jp" ) title = title.replace(" - 毎日ｊｐ(毎日新聞)", "");
     if ( host == "sankei.jp.msn.com" ) title = title.replace(" - MSN産経ニュース", "");
     if ( host == "www.asahi.com" ) title = title.replace("asahi.com（朝日新聞社）：", "");
