5f3fd2ab35515b7aa31a8112a18e58b903cd4f18
Yuya Kato <yuyakato@gmail.com>
2010-01-07 16:55:53 +0900

    記事タイトルのクレンジング処理を追加

--- ruby/lib/bookmark_utility/core.rb
+++ ruby/lib/bookmark_utility/core.rb
@@ -1,6 +1,7 @@
 
 require File.join(File.dirname(__FILE__), "canonical_table")
 require File.join(File.dirname(__FILE__), "filter_table")
+require File.join(File.dirname(__FILE__), "cleanse_title_table")
 
 module BookmarkUtility
   def self.get_canonical_url(url)
@@ -13,5 +14,11 @@ module BookmarkUtility
 
   def self.reject?(url)
     return FilterTable.any? { |pattern| pattern =~ url }
+  end
+
+  def self.cleanse_title(url, title)
+    title = title.dup
+    title.gsub!(/ - 毎日ｊｐ\(毎日新聞\)\z/, "")
+    return title
   end
 end
---
+++ ruby/test/cleanse_title_test.rb
@@ -0,0 +1,23 @@
+#! ruby -Ku
+
+$:.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
+
+require "test/unit"
+require "bookmark_utility"
+
+class CleanseTitleTest < Test::Unit::TestCase
+  def setup
+    @module = BookmarkUtility
+  end
+
+  def test_cleanse_title
+    cases = File.open(File.join(File.dirname(__FILE__), "..", "..", "cleanse_title_cases.txt"), "rb") { |file|
+      file.map { |line|
+        line.chomp.split(/\t/)
+      }
+    }
+    cases.each { |url, title, expected|
+      assert_equal(expected, @module.cleanse_title(url, title), title)
+    }
+  end
+end
