fa32966b9dfed8b0b30e2509f7322b0b5b67ff86
Yuya Kato <yuyakato@gmail.com>
2010-01-07 17:27:52 +0900

    クレンジングのパターンを追加

--- ruby/lib/bookmark_utility/cleanse_title_table.rb
+++ ruby/lib/bookmark_utility/cleanse_title_table.rb
@@ -2,6 +2,7 @@
 module BookmarkUtility
   CleanseTitleTable = {
     "mainichi.jp" => [%r'\A(.+) - 毎日ｊｐ\(毎日新聞\)\Z', '\1'].freeze,
+    "sankei.jp.msn.com" => [%r'\A(.+) - MSN産経ニュース\Z', '\1'].freeze,
     "www.asahi.com" => [%r'\Aasahi\.com（朝日新聞社）：(.+)\Z', '\1'].freeze,
     "www.yomiuri.co.jp" => [%r'\A(.+) : YOMIURI ONLINE（読売新聞）\Z', '\1'].freeze,
   }.freeze
