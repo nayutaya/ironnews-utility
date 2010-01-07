516c9c8f8f962ce9fb7b032a3c417a0651639a1f
Yuya Kato <yuyakato@gmail.com>
2010-01-07 19:38:24 +0900

    タイトルクレンジングのパターンを更新

--- javascript/lib/bookmark_utility.js
+++ javascript/lib/bookmark_utility.js
@@ -6,12 +6,16 @@ BookmarkUtility.HostPattern = /^http:\/\/(.+?)[\/:]/;
 BookmarkUtility.CleanseTitleTable = {
 //<CleanseTitleTable>
   "mainichi.jp": [/^(.+) - 毎日ｊｐ\(毎日新聞\)$/, "$1"],
-  "sankei.jp.msn.com": [/^(.+) - MSN産経ニュース$/, "$1"],
-  "www.asahi.com": [/^asahi\.com（朝日新聞社）：(.+)$/, "$1"],
-  "www.yomiuri.co.jp": [/^(.+) : YOMIURI ONLINE（読売新聞）$/, "$1"],
-  "www.47news.jp": [/^(.+) - 47NEWS（よんななニュース）$/, "$1"],
   "mytown.asahi.com": [/^asahi\.com:(.+)-マイタウン(.+)$/, "$1 - $2"],
+  "sankei.jp.msn.com": [/^(.+) - MSN産経ニュース$/, "$1"],
+  "www.47news.jp": [/^(.+) - 47NEWS（よんななニュース）$/, "$1"],
+  "www.asahi.com": [/^asahi\.com（朝日新聞社）：(.+)$/, "$1"],
   "www.chunichi.co.jp": [/^中日新聞:(.+)\(CHUNICHI Web\)$/, "$1"],
+  "www.yomiuri.co.jp": [/^(.+) : YOMIURI ONLINE（読売新聞）$/, "$1"],
+  "www.jiji.com": [/^時事ドットコム：(.+)$/, "$1"],
+  "slashdot.jp": [/^(.+) - スラッシュドット・ジャパン$/, "$1"],
+  "www.tokyo-np.co.jp": [/^東京新聞:(.+)\(TOKYO Web\)$/, "$1"],
+  "www.hokkaido-np.co.jp": [/^(.+)−北海道新聞\[青森からこんにちは\]$/, "$1"],
 //</CleanseTitleTable>
   end: null
 };
--- ruby/lib/bookmark_utility/cleanse_title_table.rb
+++ ruby/lib/bookmark_utility/cleanse_title_table.rb
@@ -2,11 +2,15 @@
 module BookmarkUtility
   CleanseTitleTable = {
     "mainichi.jp" => [%r'\A(.+) - 毎日ｊｐ\(毎日新聞\)\Z', '\1'].freeze,
-    "sankei.jp.msn.com" => [%r'\A(.+) - MSN産経ニュース\Z', '\1'].freeze,
-    "www.asahi.com" => [%r'\Aasahi\.com（朝日新聞社）：(.+)\Z', '\1'].freeze,
-    "www.yomiuri.co.jp" => [%r'\A(.+) : YOMIURI ONLINE（読売新聞）\Z', '\1'].freeze,
-    "www.47news.jp" => [%r'\A(.+) - 47NEWS（よんななニュース）\Z', '\1'].freeze,
     "mytown.asahi.com" => [%r'\Aasahi\.com:(.+)-マイタウン(.+)\Z', '\1 - \2'].freeze,
+    "sankei.jp.msn.com" => [%r'\A(.+) - MSN産経ニュース\Z', '\1'].freeze,
+    "www.47news.jp" => [%r'\A(.+) - 47NEWS（よんななニュース）\Z', '\1'].freeze,
+    "www.asahi.com" => [%r'\Aasahi\.com（朝日新聞社）：(.+)\Z', '\1'].freeze,
     "www.chunichi.co.jp" => [%r'\A中日新聞:(.+)\(CHUNICHI Web\)\Z', '\1'].freeze,
+    "www.yomiuri.co.jp" => [%r'\A(.+) : YOMIURI ONLINE（読売新聞）\Z', '\1'].freeze,
+    "www.jiji.com" => [%r'\A時事ドットコム：(.+)\Z', '\1'].freeze,
+    "slashdot.jp" => [%r'\A(.+) - スラッシュドット・ジャパン\Z', '\1'].freeze,
+    "www.tokyo-np.co.jp" => [%r'\A東京新聞:(.+)\(TOKYO Web\)\Z', '\1'].freeze,
+    "www.hokkaido-np.co.jp" => [%r'\A(.+)−北海道新聞\[青森からこんにちは\]\Z', '\1'].freeze,
   }.freeze
 end
