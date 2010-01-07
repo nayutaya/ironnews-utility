a732994fe59aa08ce1c0cd75d6dbe471569d48e4
Yuya Kato <yuyakato@gmail.com>
2010-01-07 18:07:59 +0900

    ホスト名が一致する時だけ処理するように変更

--- javascript/lib/bookmark_utility.js
+++ javascript/lib/bookmark_utility.js
@@ -2,9 +2,16 @@
 var BookmarkUtility = {};
 
 BookmarkUtility.cleanse_title = function(url, title) {
-  title = title.replace(" - 毎日ｊｐ(毎日新聞)", "");
-  title = title.replace(" - MSN産経ニュース", "");
-  title = title.replace("asahi.com（朝日新聞社）：", "");
-  title = title.replace(" : YOMIURI ONLINE（読売新聞）", "");
+console.debug(url);
+  var match = url.match(/^http:\/\/(.+?)[\/:]/);
+console.debug(match);
+  if ( match != null )
+  {
+    var host = match[1];
+    if ( host == "mainichi.jp" ) title = title.replace(" - 毎日ｊｐ(毎日新聞)", "");
+    if ( host == "sankei.jp.msn.com" ) title = title.replace(" - MSN産経ニュース", "");
+    if ( host == "www.asahi.com" ) title = title.replace("asahi.com（朝日新聞社）：", "");
+    if ( host == "www.yomiuri.co.jp" ) title = title.replace(" : YOMIURI ONLINE（読売新聞）", "");
+  }
   return title;
 };
