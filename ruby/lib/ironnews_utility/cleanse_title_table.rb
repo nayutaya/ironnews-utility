12c3000fefa8284fd05cc408aad316c9ff02bb17
Yuya Kato <yuyakato@gmail.com>
2010-01-15 12:06:48 +0900

    タイトルクレンジングパターンを更新

--- ruby/lib/ironnews_utility/cleanse_title_table.rb
+++ ruby/lib/ironnews_utility/cleanse_title_table.rb
@@ -4,17 +4,41 @@ module IronnewsUtility
     "gigazine.net" => [
       [%r'\A(.+) - GIGAZINE\Z', '\1'].freeze,
     ].freeze,
+    "headlines.yahoo.co.jp" => [
+      [%r'\A(.+) - Yahoo!ニュース\Z', '\1'].freeze,
+      [%r'\A(.+)（.+）\Z', '\1'].freeze,
+    ].freeze,
     "japan.cnet.com" => [
       [%r'\A(.+) - CNET Japan\Z', '\1'].freeze,
     ].freeze,
+    "journal.mycom.co.jp" => [
+      [%r'\A(.+) \| マイコミジャーナル\Z', '\1'].freeze,
+    ].freeze,
+    "kyushu.yomiuri.co.jp" => [
+      [%r'\A(.+) : 九州発 : YOMIURI ONLINE（読売新聞）\Z', '\1'].freeze,
+    ].freeze,
     "mainichi.jp" => [
       [%r'\A(.+) - 毎日ｊｐ\(毎日新聞\)\Z', '\1'].freeze,
+      [%r'\A(.+) － 毎日ｊｐ\(毎日新聞\)\Z', '\1'].freeze,
     ].freeze,
     "mytown.asahi.com" => [
       [%r'\Aasahi\.com:(.+)-マイタウン(.+)\Z', '\1 - \2'].freeze,
     ].freeze,
+    "news.kanaloco.jp" => [
+      [%r'\A(.+)：ローカルニュース : ニュース : カナロコ -- 神奈川新聞社\Z', '\1'].freeze,
+    ].freeze,
+    "news24.jp" => [
+      [%r'\A(.+) \| 日テレNEWS24\Z', '\1'].freeze,
+    ].freeze,
+    "osaka.yomiuri.co.jp" => [
+      [%r'\A(.+) : 関西発 : YOMIURI ONLINE（読売新聞）\Z', '\1'].freeze,
+    ].freeze,
+    "response.jp" => [
+      [%r'\A(.+) \| レスポンス自動車ニュース（Response.jp）\Z', '\1'].freeze,
+    ].freeze,
     "sankei.jp.msn.com" => [
       [%r'\A(.+) - MSN産経ニュース\Z', '\1'].freeze,
+      [%r'\A(.+?) *\(\d+/\d+ページ\)\Z', '\1'].freeze,
     ].freeze,
     "slashdot.jp" => [
       [%r'\A(.+) - スラッシュドット・ジャパン\Z', '\1'].freeze,
@@ -37,6 +61,7 @@ module IronnewsUtility
     "www.kobe-np.co.jp" => [
       [%r'\A神戸新聞｜(.+)\Z', '\1'].freeze,
       [%r'\A全国海外ニュース｜(.+)\Z', '\1'].freeze,
+      [%r'\A事件・事故｜(.+)\Z', '\1'].freeze,
     ].freeze,
     "www.tokyo-np.co.jp" => [
       [%r'\A東京新聞:(.+)\(TOKYO Web\)\Z', '\1'].freeze,
