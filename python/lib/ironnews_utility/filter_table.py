a03e904555e3bdbbc033d99cdb0039a698042080
Yuya Kato <yuyakato@gmail.com>
2010-01-20 00:49:16 +0900

    フィルタパターンを更新

--- python/lib/ironnews_utility/filter_table.py
+++ python/lib/ironnews_utility/filter_table.py
@@ -8,6 +8,7 @@ FilterTable = (
   re.compile(r'\Ahttp://car\.nikkei\.co\.jp/release/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://cargo-news\.co\.jp/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://contents\.innolife\.net/'), # 理由: title要素に記事名が含まれていないため
+  re.compile(r'\Ahttp://gendai\.net/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://japan\.donga\.com/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://japanese\.donga\.com/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://jp\.reuters\.com/'), # 理由: 記事名の取得に失敗することが多く、かつ鉄道関連の記事が少ないため
@@ -19,6 +20,7 @@ FilterTable = (
   re.compile(r'\Ahttp://release\.nikkei\.co\.jp/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://rkk\.jp/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://www2\.knb\.ne\.jp/'), # 理由: title要素に記事名が含まれていないため
+  re.compile(r'\Ahttp://www\.asahi\.com/english/weekly/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://www\.chemicaldaily\.co\.jp/news/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://www\.chosunonline\.com/'), # 理由: 記事の閲覧が有料であるため
   re.compile(r'\Ahttp://www\.chugoku-np\.co\.jp/News/Tn'), # 理由: title要素に記事名が含まれていないため
@@ -28,12 +30,14 @@ FilterTable = (
   re.compile(r'\Ahttp://www\.keidanren\.or\.jp/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://www\.kentsu\.co\.jp/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://www\.mbs\.jp/news/'), # 理由: 記事の閲覧可能期間が短いため
+  re.compile(r'\Ahttp://www\.muromin\.mnw\.jp/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://www\.mxtv\.co\.jp/mxnews/news/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://www\.nikkei\.co\.jp/kaigai/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://www\.nikkei\.co\.jp/kyushu/news/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://www\.nikkei\.co\.jp/news/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://www\.pjnews\.net/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://www\.portfolio\.nl/'), # 理由: title要素に記事名が含まれていないため
+  re.compile(r'\Ahttp://www\.rkk\.co\.jp/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://www\.sanpo-pub\.co\.jp/column/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://www\.shonai-nippo\.co\.jp/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://www\.tku\.co\.jp/pc/news/'), # 理由: title要素に記事名が含まれていないため
@@ -42,5 +46,6 @@ FilterTable = (
   re.compile(r'\Ahttp://www\.travelvision\.jp/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://www\.usfl\.com/Daily/News/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://www\.wbs\.co\.jp/'), # 理由: title要素に記事名が含まれていないため
+  re.compile(r'\Ahttp://www\.worldtimes\.co\.jp/today/'), # 理由: title要素に記事名が含まれていないため
   re.compile(r'\Ahttp://www\.xinhua\.jp/'), # 理由: 記事の閲覧が有料であるため
 )
--- ruby/lib/ironnews_utility/filter_table.rb
+++ ruby/lib/ironnews_utility/filter_table.rb
@@ -6,6 +6,7 @@ module IronnewsUtility
     %r'\Ahttp://car\.nikkei\.co\.jp/release/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://cargo-news\.co\.jp/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://contents\.innolife\.net/', # 理由: title要素に記事名が含まれていないため
+    %r'\Ahttp://gendai\.net/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://japan\.donga\.com/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://japanese\.donga\.com/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://jp\.reuters\.com/', # 理由: 記事名の取得に失敗することが多く、かつ鉄道関連の記事が少ないため
@@ -17,6 +18,7 @@ module IronnewsUtility
     %r'\Ahttp://release\.nikkei\.co\.jp/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://rkk\.jp/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://www2\.knb\.ne\.jp/', # 理由: title要素に記事名が含まれていないため
+    %r'\Ahttp://www\.asahi\.com/english/weekly/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://www\.chemicaldaily\.co\.jp/news/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://www\.chosunonline\.com/', # 理由: 記事の閲覧が有料であるため
     %r'\Ahttp://www\.chugoku-np\.co\.jp/News/Tn', # 理由: title要素に記事名が含まれていないため
@@ -26,12 +28,14 @@ module IronnewsUtility
     %r'\Ahttp://www\.keidanren\.or\.jp/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://www\.kentsu\.co\.jp/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://www\.mbs\.jp/news/', # 理由: 記事の閲覧可能期間が短いため
+    %r'\Ahttp://www\.muromin\.mnw\.jp/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://www\.mxtv\.co\.jp/mxnews/news/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://www\.nikkei\.co\.jp/kaigai/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://www\.nikkei\.co\.jp/kyushu/news/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://www\.nikkei\.co\.jp/news/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://www\.pjnews\.net/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://www\.portfolio\.nl/', # 理由: title要素に記事名が含まれていないため
+    %r'\Ahttp://www\.rkk\.co\.jp/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://www\.sanpo-pub\.co\.jp/column/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://www\.shonai-nippo\.co\.jp/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://www\.tku\.co\.jp/pc/news/', # 理由: title要素に記事名が含まれていないため
@@ -40,6 +44,7 @@ module IronnewsUtility
     %r'\Ahttp://www\.travelvision\.jp/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://www\.usfl\.com/Daily/News/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://www\.wbs\.co\.jp/', # 理由: title要素に記事名が含まれていないため
+    %r'\Ahttp://www\.worldtimes\.co\.jp/today/', # 理由: title要素に記事名が含まれていないため
     %r'\Ahttp://www\.xinhua\.jp/', # 理由: 記事の閲覧が有料であるため
   ].freeze
 end
